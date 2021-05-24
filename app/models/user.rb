7eeb947166358d2320af418d39f84eb5b5f3d41c
rubyboy-hub <shanjoy.shajan@rubyians.com>
2021-05-24 17:16:27 +0530

    association corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   belongs_to :role
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        #  enum role_id: { Student: 2, Teacher: 3,Parent: 4 } 
-       
-         
+         :recoverable, :rememberable, :validatable   
 end
