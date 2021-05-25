8d6023b6dd25639f72ee7a9f9de5e4ff083fac48
rubyboy-hub <shanjoy.shajan@rubyians.com>
2021-05-25 19:29:52 +0530

    added association for user,comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   belongs_to :role
   belongs_to :standard
   has_many :subjects, through: :standard
+  has_many :parents, class_name: "User",foreign_key: "parent_id"
+  belongs_to :Parent, class_name: "User", optional: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable   
